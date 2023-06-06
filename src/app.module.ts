0a762e0fa187c2fcebf4be000b3c2e4c9d56c3d2
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-06-07 01:38:14 +0200

    build: add build scripts for frontend and backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { RemixController } from './remix/remix.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../public'),
+      serveStaticOptions: {
+        immutable: true,
+        maxAge: '1y',
+      },
+    }),
+  ],
   controllers: [RemixController],
   providers: [AppService],
 })
